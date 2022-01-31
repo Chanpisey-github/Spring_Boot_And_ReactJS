ae27759eb0b8e11332a137a79fce4d01dd9f89b6
HOUR Chan Pisey <77218571+Chanpisey-github@users.noreply.github.com>
2022-01-31 20:03:12 +0700

    Add RestAPI of Team

---
+++ src/main/java/org/pisey/hour/model/Team.java
@@ -0,0 +1,81 @@
+package org.pisey.hour.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class Team {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String teamName;
+	private long totalMatches;
+	private long totalWins;
+
+	@Transient
+	private List<Match> matches;
+
+	public List<Match> getMatches() {
+		return matches;
+	}
+
+	public void setMatches(List<Match> matches) {
+		this.matches = matches;
+	}
+
+	public Team() {
+		
+	}
+
+
+	public Team(String teamName, long totalMatches) {
+		super();
+		this.teamName = teamName;
+		this.totalMatches = totalMatches;
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public long getTotalMatches() {
+		return totalMatches;
+	}
+
+	public void setTotalMatches(long totalMatches) {
+		this.totalMatches = totalMatches;
+	}
+
+	public long getTotalWins() {
+		return totalWins;
+	}
+
+	public void setTotalWins(long totalWins) {
+		this.totalWins = totalWins;
+	}
+
+	@Override
+	public String toString() {
+		return "Team [teamName=" + teamName + ", totalMatches=" + totalMatches + ", totalWins=" + totalWins + "]";
+	}
+
+}
---
+++ src/main/java/org/pisey/hour/repository/MatchRepository.java
@@ -0,0 +1,18 @@
+package org.pisey.hour.repository;
+
+import java.util.List;
+
+import org.pisey.hour.model.Match;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface MatchRepository extends CrudRepository<Match, Long> {
+	
+	List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
+	
+	  default List<Match> findLatestMatchesbyTeam(String teamName, int count) {
+	        return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
+	    }
+}
